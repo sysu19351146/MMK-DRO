f81be98c8efeaca27499c1130b33f3adab9df5ac
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2024-02-03 19:50:29 +0800

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,18 +1,2 @@
-from dataloaders.MosMed_L import MosMed_L
 from dataloaders.SARS_COV_2 import SARS_COV_2
-from dataloaders.COV_C import COV_C
-from dataloaders.MosMed_M import MosMed_M
-from dataloaders.COV_R import COV_R
-from dataloaders.COV_I import COV_I
-from dataloaders.COV_M import COV_M
-from dataloaders.MNIST import MNIST
-from dataloaders.skindata import skindata
-from dataloaders.HAM10000 import HAM10000
-from dataloaders.PAD import PAD
-from dataloaders.ourskin import ourskin
-from dataloaders.PH2D import PH2D
-from dataloaders.ISIC2017 import ISIC2017
-from dataloaders.ISIC2019nv import ISIC2019nv
-from dataloaders.ISIC2019bcn import ISIC2019bcn
-
-__all__ = ["SARS_COV_2", "MosMed_M", "MosMed_L", "COV_R", "COV_C", "COV_I", "COV_M",'MNIST','skindata','HAM10000','PAD','ourskin','PH2D','ISIC2017','ISIC2019nv','ISIC2019bcn']
+__all__ = ["SARS_COV_2"]
